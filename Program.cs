17ad52daed75060abcb9953bf062755899729444
harD1kkkk <137618635+harD1kkkk@users.noreply.github.com>
2024-01-26 17:47:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Myspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> intStack = new Stack<int>(5);
+            intStack.Push(10);
+            intStack.Push(20);
+            intStack.Push(30);
+            Console.WriteLine("Peek: " + intStack.Peek());
+            Console.WriteLine("Pop: " + intStack.Pop());
+            Console.WriteLine("Peek: " + intStack.Peek());
+            Console.WriteLine();
+
+            Stack<string> stringStack = new Stack<string>(3);
+            stringStack.Push("Hello");
+            stringStack.Push("World");
+            Console.WriteLine("Peek: " + stringStack.Peek());
+            Console.WriteLine("Pop: " + stringStack.Pop());
+            Console.WriteLine("Peek: " + stringStack.Peek());
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,46 @@
+﻿public class Stack<T>
+{
+    private T[] elements;
+    private int top;
+
+    public Stack(int size)
+    {
+        elements = new T[size];
+        top = -1;
+    }
+
+    public void Push(T item)
+    {
+        if (top == elements.Length - 1)
+        {
+            Console.WriteLine("Stack overflow!");
+        }
+        else
+        {
+            top++;
+            elements[top] = item;
+        }
+    }
+
+    public T Pop()
+    {
+        if (top == -1)
+        {
+            throw new InvalidOperationException("Stack is empty!");
+        }
+
+        T result = elements[top];
+        top--;
+        return result;
+
+    }
+
+    public T Peek()
+    {
+        if (top == -1)
+        {
+            throw new InvalidOperationException("Stack is empty!");
+        }
+        return elements[top];
+    }
+}
